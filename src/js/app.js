6dbadb0abfa58aee2a2bb367ba3850feec5af469
bruno santos <devbrunosantos@gmail.com>
2022-05-01 23:21:00 -0800

    feat: initialize the map with location fixed

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,28 @@ import myKey from '../../myKeyAPI'
 
 // to work, comment the top line and add your key as the value of the line bottom(and uncomment, of course)
 // let myKey = 'YOUR_KEY'
+
+// Initialize and add the map
+function initMap() {
+  // The location of locationInitial
+  const locationInitial = { lat: 43.65107, lng: -79.347015 }
+  // The map, centered at locationInitial
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: locationInitial
+  })
+  // The marker, positioned at locationInitial
+  const marker = new google.maps.Marker({
+    position: locationInitial,
+    map: map
+  })
+}
+
+window.initMap = initMap
+
+const linkMap = `<script
+src="https://maps.googleapis.com/maps/api/js?key=${myKey}&callback=initMap"
+async
+defer
+></script>`
+$('body').append(linkMap)
